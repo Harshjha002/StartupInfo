6bdff1c4e24e37ca143e48a323d8ef22de4a5233
Harsh <harshjha8453@gmail.com>
2024-11-26 22:20:55 +0530

    added sanity && some UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { STARTUP_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({searchParams} : {searchParams:Promise<{query?:string}>}) {
 
-  const query = (await searchParams).query
+  const query = (await searchParams).query;
+  const params = {search:query || null}
+
+  // const post = await client.fetch(STARTUP_QUERY)
+  const {data:post} = await sanityFetch({query:STARTUP_QUERY,params})
+
 
   return (
     <>
@@ -15,6 +23,20 @@ export default async function Home({searchParams} : {searchParams:Promise<{query
         </p>
         <SearchForm query={query}/>
       </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search result for "${query}"` : 'All startups '}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {post?.length > 0 ? ( post.map((post:StartupTypeCard) => (  <StartupCard key={post?._id} post={post}/> ))):(
+            <p className="">No Startups found</p>
+          )}
+        </ul>
+
+      </section>
+
+      <SanityLive/>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,20 +15,19 @@ const config: Config = {
       },
       colors: {
         primary: {
-          DEFAULT: "#024CAA",
-          light: "#91B7E3",
+          "100": "#FFE8F0",
+          DEFAULT: "#EE2B69",
         },
-        accent: {
-          DEFAULT: "#EC8305",
-          light: "#F5B14D",
+        secondary: "#FBE843",
+        black: {
+          "100": "#333333",
+          "200": "#141413",
+          "300": "#7D8087",
+          DEFAULT: "#000000",
         },
-        dark: {
-          DEFAULT: "#091057",
-          light: "#202C6D",
-        },
-        neutral: {
-          DEFAULT: "#DBD3D3",
-          dark: "#AFAFAF",
+        white: {
+          "100": "#F7F7F7",
+          DEFAULT: "#FFFFFF",
         },
       },
       fontFamily: {
@@ -42,7 +41,7 @@ const config: Config = {
       boxShadow: {
         100: "2px 2px 0px 0px rgb(0, 0, 0)",
         200: "2px 2px 0px 2px rgb(0, 0, 0)",
-        300: "2px 2px 0px 2px rgb(2, 76, 170)",
+        300: "2px 2px 0px 2px rgb(238, 43, 105)",
       },
     },
   },
